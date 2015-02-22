b93385a6b4a862ba5a8ce4fa7b8627057e39676c
khusika <khusikadhamar@gmail.com>
2015-02-23 04:37:39 +0700

    Changes for bs-lp

--- cm.mk
+++ cm.mk
@@ -1,8 +1,12 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common BeanStalk stuff.
+$(call inherit-product, vendor/beanstalk/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/full_ancora.mk)
+
+# boot animation
+PRODUCT_COPY_FILES += \
+    vendor/beanstalk/prebuilt/common/bootanimation/480.zip:system/media/bootanimation.zip
 
 # This device is hdpi
 PRODUCT_AAPT_CONFIG := normal hdpi
@@ -11,7 +15,7 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 PRODUCT_RELEASE_NAME := ancora
 
 # Setup device configuration
-PRODUCT_NAME := cm_ancora
+PRODUCT_NAME := bs_ancora
 PRODUCT_DEVICE := ancora
 PRODUCT_BRAND := Samsung
 PRODUCT_MANUFACTURER := Samsung
